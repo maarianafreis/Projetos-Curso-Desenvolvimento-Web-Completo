266b02559a6e84f1d0547458c63d6e9225fef4cb
Mariana Reis <marianafreitas_reis@hotmail.com>
2021-11-24 16:24:56 -0300

    add projeto php 7

---
+++ 8 PHP 7/app_help_desk/logoff.php
@@ -0,0 +1,29 @@
+<?php
+	session_start();
+
+	/*echo '<pre>';
+	print_r($_SESSION);
+	echo '</pre>';
+
+	//remover índices do array de sessão
+	//unset()
+
+	unset($_SESSION['x']);
+
+	echo '<pre>';
+	print_r($_SESSION);
+	echo '</pre>';
+
+	//destruir a variável de sessão
+	//session_destroy()
+
+	session_destroy();
+
+	echo '<pre>';
+	print_r($_SESSION);
+	echo '</pre>';
+	*/
+
+	session_destroy();
+	header('Location: index.php');
+?>
